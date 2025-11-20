19642dffd8c6e556720a926e3821aadc821e408e
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2025-11-20 14:38:42 -0300

    Atualizar o LemuroidGameImage.kt

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/shared/compose/ui/LemuroidGameImage.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/shared/compose/ui/LemuroidGameImage.kt
@@ -1,10 +1,15 @@
 package com.swordfish.lemuroid.app.mobile.shared.compose.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.AsyncImage
@@ -25,18 +30,35 @@ fun LemuroidGameImage(
 
     val fallbackPainter = rememberDrawablePainter(drawable = fallbackDrawable)
 
-    AsyncImage(
-        model =
-            ImageRequest.Builder(LocalContext.current)
-                .data(game.coverFrontUrl)
-                .build(),
-        contentDescription = game.title,
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .aspectRatio(1.0f),
-        fallback = fallbackPainter,
-        error = fallbackPainter,
-        contentScale = ContentScale.Crop,
-    )
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(1.0f)
+    ) {
+        AsyncImage(
+            model =
+                ImageRequest.Builder(LocalContext.current)
+                    .data(game.coverFrontUrl)
+                    .build(),
+            contentDescription = game.title,
+            modifier = Modifier.fillMaxSize(),
+            fallback = fallbackPainter,
+            error = fallbackPainter,
+            contentScale = ContentScale.Crop,
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black.copy(alpha = 0.1f),
+                            Color.Black.copy(alpha = 0.4f)
+                        )
+                    )
+                )
+        )
+    }
 }
