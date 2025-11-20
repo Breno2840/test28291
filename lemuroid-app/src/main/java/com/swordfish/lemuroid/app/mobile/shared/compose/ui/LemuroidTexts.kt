3a2fbc46bd89e5554ee73604162e2cc8662755ab
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2025-11-20 14:50:03 -0300

    Atualizar o LemuroidTexts.kt

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/shared/compose/ui/LemuroidTexts.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/shared/compose/ui/LemuroidTexts.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.swordfish.lemuroid.app.utils.games.GameUtils
@@ -34,17 +35,20 @@ fun LemuroidTexts(
     subtitle: String,
 ) {
     Column(
-        modifier = modifier.padding(8.dp),
+        modifier = modifier.padding(10.dp),
     ) {
         Text(
             text = title,
             style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onSecondaryContainer,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
         )
         Text(
             text = subtitle,
             style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
         )
